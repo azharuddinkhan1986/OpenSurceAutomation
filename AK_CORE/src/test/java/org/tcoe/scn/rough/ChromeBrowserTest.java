7dc39d6ef93f20015c87c80134cefaaf23da26d3
Azhar <azhar.sqa@gmail.com>
2018-03-04 20:32:03 +0530

    Adding Thread.sleep to wait for browser to load AUT

--- AK_CORE/src/test/java/org/tcoe/scn/rough/ChromeBrowserTest.java
+++ AK_CORE/src/test/java/org/tcoe/scn/rough/ChromeBrowserTest.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeBrowserTest {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.gmail.com");
+		Thread.sleep(5000);
 		driver.quit();
 	}
 
