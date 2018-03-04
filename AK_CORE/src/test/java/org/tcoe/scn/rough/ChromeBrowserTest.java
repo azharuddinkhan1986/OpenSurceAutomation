728d803af2ccf238e6fc4318bc5279a6f2ecb153
AZHARUDDIN KHAN <azhar.sqa@gmail.com>
2018-03-05 01:25:08 +0530

    Added All dependencies and Created Project Structure

--- AK_CORE/src/test/java/org/tcoe/scn/rough/ChromeBrowserTest.java
+++ AK_CORE/src/test/java/org/tcoe/scn/rough/ChromeBrowserTest.java
@@ -2,15 +2,20 @@ package org.tcoe.scn.rough;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class ChromeBrowserTest {
 	
-	public static void main(String[] args) throws InterruptedException {
+	
+	@Test
+	public void chromeBrowserTest() throws InterruptedException
+	{
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.gmail.com");
 		Thread.sleep(5000);
 		driver.quit();
 	}
+	
 
 }
